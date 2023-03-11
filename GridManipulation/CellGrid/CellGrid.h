9e5e2aab14c0717cc5470dd068ae9ba1c0140088
PaulOthar <xpaul.e.j@gmail.com>
2023-03-11 14:42:20 -0300

    added general grid functionality

--- GridManipulation/CellGrid/CellGrid.h
+++ GridManipulation/CellGrid/CellGrid.h
@@ -7,17 +7,13 @@ typedef struct /*Cell Grid*/{
     int* cellValue;
 }CellGrid;
 
-//Basic Manipulation
-
 extern CellGrid* new_CellGrid(int width,int height);
 
 extern void free_CellGrid(CellGrid* input);
 
-//Basic Functionality
+extern int readSlot_CellGrid(CellGrid* input,int x,int y);
 
-extern int readSlot_CellGrid(CellGrid* input);
-
-extern int readAreaVacancy_CellGrid(CellGrid* input,int x1,int y1,int x2,int y2);
+extern int readAreaVacancy_CellGrid(CellGrid* input,int x1,int y1,int x2,int y2,int emptyValue);
 
 extern void writeSlot_CellGrid(CellGrid* input,int x,int y,int value);
 
